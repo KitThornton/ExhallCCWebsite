ea0086a1bfb44a844ebb703b4235029b2e61e646
kitthornton <kit.thornton16@gmail.com>
2021-11-13 12:44:06 +0000

    total project refactor

--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'),
-    router = express.Router(),
-    pool = require("../DB.js");
-
-/* GET home page. */
-app.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
